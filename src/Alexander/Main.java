526a81f6ef339f520a8f2ae1a029fe2452f3b095
Alexander Schaedlich <alexander.schaedlich@gmail.com>
2020-10-28 15:21:03 +0100

    string exercise

--- src/Alexander/Main.java
+++ src/Alexander/Main.java
@@ -1,7 +1,23 @@
 package Alexander;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //String string = "Was it a car or a cat I saw?";
+        //System.out.println(string.length());
+        //System.out.println(string.substring(9, 12));
+        //System.out.println(string.split());
 
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your full name:");
+
+        //String output = "";
+
+        while (input.hasNext()) {
+            //output += input.next().charAt(0) + "-";
+            System.out.print(input.next().charAt(0));
+        }
     }
 }
